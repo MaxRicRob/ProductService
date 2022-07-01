369481cfecb97b7c85f6402f1b47a74facb993dd
maxim.irmscher <maxim.irmscher@idealo.de>
2022-07-01 12:15:04 +0200

    KK-64-2 implement oneQueueForAll

--- src/main/java/com/example/ProductServiceApplication/api/RabbitController.java
+++ src/main/java/com/example/ProductServiceApplication/api/RabbitController.java
@@ -5,6 +5,9 @@ import com.google.gson.Gson;
 import org.springframework.amqp.core.Message;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+
+import java.nio.charset.StandardCharsets;
 
 
 public class RabbitController {
@@ -12,21 +15,26 @@ public class RabbitController {
     @Autowired
     private ProductService productService;
 
-    @RabbitListener(queues = "${queue-names.components}")
-    public String getAllProductComponents() {
+    @RabbitListener(queues = "${queue-names.product-service}")
+    public String getAllProductComponents(Message message) {
 
-        return new Gson().toJson(productService.getAllProductComponents());
+        var input = new String(message.getBody(), StandardCharsets.UTF_8);
+
+        if (input.equals("getComponents")) {
+            return new Gson().toJson(productService.getAllProductComponents());
+        }
+        if (input.equals("getDefaultProducts")) {
+            return new Gson().toJson(productService.getAllDefaultProducts());
+        }
+        return null;
+
+
     }
 
-    @RabbitListener(queues = "${queue-names.default-products}")
-    public String getAllDefaultProducts() {
-
-        return new Gson().toJson(productService.getAllDefaultProducts());
-    }
 
     @RabbitListener(queues = "${queue-names.user-products}")
     public String getUserProducts(Message message) {
-        
+
         return new Gson().toJson(productService.getAllProductsFromUser(message.toString()));
     }
 
