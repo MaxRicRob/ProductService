050e6a811c7927e12385c072551abd325e1949b8
maxim.irmscher <maxim.irmscher@idealo.de>
2022-07-07 15:58:17 +0200

    implement redis cache

--- src/main/java/com/example/ProductServiceApplication/repository/ProductComponentRepositoryImpl.java
+++ src/main/java/com/example/ProductServiceApplication/repository/ProductComponentRepositoryImpl.java
@@ -3,19 +3,25 @@ package com.example.ProductServiceApplication.repository;
 import com.example.ProductServiceApplication.entity.ProductComponent;
 import com.example.ProductServiceApplication.repository.jpa.ProductComponentEntityJpaRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.stream.Collectors;
 
+@Slf4j
 @Repository
 @RequiredArgsConstructor
 public class ProductComponentRepositoryImpl implements ProductComponentRepository {
 
     private final ProductComponentEntityJpaRepository productComponentEntityJpaRepository;
 
+    @Cacheable(value="productComponentCache")
     @Override
     public List<ProductComponent> findAll() {
+        log.info("get components from repo without cache");
+
         return productComponentEntityJpaRepository.findAll().stream()
                 .map(ProductComponent::from)
                 .collect(Collectors.toList());
